6389c3b27dc126f96cdf91ab301e175215cde2ca
Persephone Wilder <shaneawilder@gmail.com>
2025-03-09 19:11:58 -0600

    Updated Conversion Calculator.py to run GUI

--- Conversion Calculator.py
+++ Conversion Calculator.py
@@ -1,4 +1,10 @@
 from DCGUI import CalculatorGUI as gui
+from PySide6 import QtWidgets
+import sys
 
 if __name__ == '__main__':
-        gui()
+        app = QtWidgets.QApplication([])
+        widget = gui()
+        widget.show()
+        sys.exit(app.exec())
+        
